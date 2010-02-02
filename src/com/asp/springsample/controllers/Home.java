6986b446b8e3002036454109afd2d9edc8d24dbe
Arun Shanker Prasad <arunshankerprasad+github@gmail.com>
2010-02-03 03:37:56 +0530

    Initial

---
+++ src/com/asp/springsample/controllers/Home.java
@@ -0,0 +1,16 @@
+package com.asp.springsample.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello/hello";
+	}
+}
